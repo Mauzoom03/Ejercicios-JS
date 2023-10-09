7e3c5924e37b4ff8dbfca12611c8dd32a3bccf0b
Mauzoom <mauro.quintana@bootcamp-upgrade.com>
2023-10-09 11:04:39 +0200

    ex1 conditionals

--- S1/4.conditionals/exercise-1.js
+++ S1/4.conditionals/exercise-1.js
@@ -15,7 +15,7 @@ if (number1 !== number2) {
   console.log("number1 es estrictamente distinto a number2");
 }
 
-if (number3 !== number1) {
+if (number3 != number1) {
   console.log("number3 es distinto number1");
 }
 
@@ -23,10 +23,10 @@ if (number3 * 5 === number1) {
   console.log("number3 por 5 es igual a number1");
 }
 
-if (number1 * 2 === number2) {
+if (number3 * 5 === number1 && number1 * 2 === number2) {
   console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
 }
 
-if (number2 / 2 === number1) {
+if (number2 / 2 === number1 || number1 / 5 === number3) {
   console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
 }
